37e8a8e8f5b63bbe1f07f2b69cc23b566a5af6c3
Patrick Purcell <p.84@live.com>
2017-05-30 04:52:41 -0700

    Updating Window

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,28 @@
 
+/*
+
+PW : Platform Windows
+PL : Platform Linux
+
+sudo apt-get install libx11-dev
+reset; g++ -std=c++11 -Wall Main.cpp -o MiniWindow -lX11; ./MiniWindow
+
+*/
+
 #include <cmath>
 #include <thread>
 #include <cstdint>
 #include <cstddef>
-
+#include <cstdint>
+#include <cstddef>
 #include <iostream>
+
+#include <array>
+#include <vector>
 
 #if defined(_WIN32) || defined(_WIN64)
 #define WIN_32_LEAN_AND_MEAN
-#define NOMMINMAX
+#define NOMINMAX
 #define PW (1)
 #include<Windows.h>
 #endif
@@ -18,99 +32,154 @@
 #include <X11/Xlib.h>
 #endif
 
-#define A auto
-#define U uint32_t
+#include "Defines.hpp"
+#include "MiniGLM.hpp"
+
+using namespace std;
+using namespace glm;
+
+#define GOLF
 
 #if PW
-LRESULT CALLBACK wp(HWND h, UINT message, WPARAM wparam, LPARAM lparam);
-#endif
-
-struct W
+#ifndef GOLF
+struct FB
 {
-    U w;
-    U h;
-    bool o { true };
+    U w,h;
+    HDC d{0};
+    HBITMAP b{0};
+    COLORREF*c;
 
-    #if PW
-    HDC dc;
-    HWND hw;
-
-    W(U w, U h)
-        : w { w }
-        , h { h }
+    ~FB()
     {
-        U px = 0;
-        U py = 0;
-
-        A i = GetModuleHandle(0);
-        WNDCLASSEX c { };
-        c.cbSize = sizeof(c);
-        c.hInstance = i;
-        c.style = CS_HREDRAW | CS_VREDRAW;
-        c.lpszClassName = "Window Class Name";
-        c.lpfnWndProc = wp;
-        c.hCursor = LoadCursor(0, IDC_ARROW);
-        c.hIconSm = LoadIcon(i, IDI_APPLICATION);
-        c.hIcon = LoadIcon(i, IDI_APPLICATION);
-        c.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-        RegisterClassEx(&c);
-
-        RECT r { 0, 0, (LONG)w, (LONG)h };
-        hw = CreateWindowEx(
-            NULL,
-            "Window Class Name",
-            "Window Title",
-            // HACK : Removing anything that allows the Window to resize...
-            // FUCK : Resizing the Window crashes the app...
-            WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME,
-            px,
-            py,
-            r.right - r.left,
-            r.bottom - r.top,
-            NULL,
-            NULL,
-            i,
-            NULL
-        );
-
-        SetWindowLongPtr(hw, GWL_USERDATA, (LONG)this);
-        dc = GetDC(hw);
-
-        BOOL b;
-        b = ShowWindow(hw, SW_SHOW);
-        b = SetForegroundWindow(hw);
-        SetFocus(hw);
+        if(d)DeleteDC(d);
+        if(b)DeleteObject(b);
     }
 
-    void u()
+    V s(HDC g,U x,U y)
     {
-        MSG m;
-        if (PeekMessage(&m, hw, 0, 0, PM_REMOVE)) {
-            TranslateMessage(&m);
+        this->~FB();
+        w=x;
+        h=y;
+        d=CreateCompatibleDC(g);
+        BITMAPINFO i{sizeof(i),int(w),-int(h),1,32,BI_RGB,0,0,0,0,{}};
+        b=CreateDIBSection(g,&i,DIB_RGB_COLORS,(V**)&c,0,0);
+        SelectObject(d,b);
+    }
+
+    V p(U x,U y,vec3 v)
+    {
+        v=saturate(v);
+        v=v*255;
+        c[y*w+x]=RGB(B(v[2]),B(v[1]),B(v[0]));
+    }
+};
+
+LRESULT CALLBACK wp(HWND h,UINT message,WPARAM wparam,LPARAM lparam);
+
+struct WN
+{
+    U w,h,o{1};
+    HWND _;
+    HDC d;
+    FB f;
+    MSG m;
+    void(*s)(float);
+    WN(U w,U h)
+        :w{w}
+        ,h{h}
+    {
+        A i=GetModuleHandle(0);
+        WNDCLASSEX c{sizeof(WNDCLASSEX),0,wp,0,0,i,0,0,0,0,"-",0};
+        RegisterClassEx(&c);
+        _=CreateWindowEx(0,"-",0,WS_MINIMIZEBOX|WS_MAXIMIZEBOX|WS_SYSMENU|WS_THICKFRAME,0,0,w,h,0,0,i,0);
+        RECT wr,cr;
+        GetWindowRect(_,&wr);
+        GetClientRect(_,&cr);
+        MoveWindow(_,wr.left,wr.top,w+wr.right-wr.left-cr.right,h+wr.bottom-wr.top-cr.bottom,0);
+        d=GetDC(_);
+        f.s(d,w,h);
+        SetWindowLongPtr(_,GWLP_USERDATA,(LONG_PTR)this);
+        ShowWindow(_,SW_SHOW);
+        SetForegroundWindow(_);
+        SetFocus(_);
+    }
+
+    V u()
+    {
+        while(PeekMessage(&m,_,0,0,PM_REMOVE))
+            TranslateMessage(&m),
             DispatchMessage(&m);
+        InvalidateRect(_, 0, 0);
+    }
+
+    V c()
+    {
+        ReleaseDC(_,d);
+        DeleteDC(d);
+        DestroyWindow(_);
+        o=0;
+    }
+};
+
+LRESULT CALLBACK wp(HWND h,UINT m,WPARAM wp,LPARAM lp)
+{
+    A w=(WN*)GetWindowLongPtr(h,GWLP_USERDATA);
+    if(w){
+        A&_=*w;
+        switch(m){
+            case WM_CLOSE:_.c();break;
+            case WM_SIZE:{
+                _.w=LOWORD(lp);
+                _.h=HIWORD(lp);
+                _.f.s(_.d,_.w,_.h);
+            }break;
+            case WM_MOUSEWHEEL:_.s(short(HIWORD(wp)));break;
+            case WM_PAINT:{
+                PAINTSTRUCT s;
+                A hdc=BeginPaint(_._,&s);
+                BitBlt(hdc,0,0,_.w,_.h,_.f.d,0,0,SRCCOPY);
+                EndPaint(_._,&s);
+            }break;
         }
     }
+    R DefWindowProc(h,m,wp,lp);
+}
+#else
+/////////////////////////////////////////////////////////////////////////////////////////
+struct FB{U w,h;HDC d{0};HBITMAP b{0};COLORREF*c;~FB(){if(d)DeleteDC(d);if(b)
+DeleteObject(b);}V s(HDC g,U x,U y){this->~FB();w=x;h=y;d=CreateCompatibleDC(g);
+BITMAPINFO i{sizeof(i),int(w),-int(h),1,32,BI_RGB,0,0,0,0,{}};b=CreateDIBSection(g,&i,
+DIB_RGB_COLORS,(V**)&c,0,0);SelectObject(d,b);}V p(U x,U y,vec3 v){v=saturate(v);v=v*255;
+c[y*w+x]=RGB(B(v[2]),B(v[1]),B(v[0]));}};LRESULT CALLBACK wp(HWND h,UINT message,WPARAM
+wparam,LPARAM lparam);struct WN{U w,h,o{1};HWND _;HDC d;FB f;MSG m;void(*s)(float);WN(U w
+,U h):w{w},h{h}{A i=GetModuleHandle(0);WNDCLASSEX c{sizeof(WNDCLASSEX),0,wp,0,0,i,0,0,0,0
+,"-",0};RegisterClassEx(&c);_=CreateWindowEx(0,"-",0,WS_MINIMIZEBOX|WS_MAXIMIZEBOX|
+WS_SYSMENU|WS_THICKFRAME,0,0,w,h,0,0,i,0);RECT wr,cr;GetWindowRect(_,&wr);GetClientRect(_
+,&cr);MoveWindow(_,wr.left,wr.top,w+wr.right-wr.left-cr.right,h+wr.bottom-wr.top-cr.
+bottom,0);d=GetDC(_);f.s(d,w,h);SetWindowLongPtr(_,GWLP_USERDATA,(LONG_PTR)this);
+ShowWindow(_,SW_SHOW);SetForegroundWindow(_);SetFocus(_);}V u(){while(PeekMessage(&m,_,0,
+0,PM_REMOVE))TranslateMessage(&m),DispatchMessage(&m);InvalidateRect(_, 0, 0);}V c(){
+ReleaseDC(_,d);DeleteDC(d);DestroyWindow(_);o=0;}};LRESULT CALLBACK wp(HWND h,UINT m,
+WPARAM wp,LPARAM lp){A w=(WN*)GetWindowLongPtr(h,GWLP_USERDATA);if(w){A&_=*w;switch(m){
+case WM_CLOSE:_.c();break;case WM_SIZE:{_.w=LOWORD(lp);_.h=HIWORD(lp);_.f.s(_.d,_.w,_.h);
+}break;case WM_MOUSEWHEEL:_.s(short(HIWORD(wp)));break;case WM_PAINT:{PAINTSTRUCT s;A hdc
+=BeginPaint(_._,&s);BitBlt(hdc,0,0,_.w,_.h,_.f.d,0,0,SRCCOPY);EndPaint(_._,&s);}break;}}R
+DefWindowProc(h,m,wp,lp);}
+/////////////////////////////////////////////////////////////////////////////////////////
+#endif
+#endif
 
-    void r()
-    {
-
-    }
-
-    void c()
-    {
-        ReleaseDC(hw, dc);
-        DeleteDC(dc);
-        DestroyWindow(hw);
-        o = false;
-    }
-    #elif PL
+#if PL
+struct WN
+{
+    U w,h,o{1};
     int s;
     GC gc;
     XEvent e;
     Window hw;
     Display* d;
     
-    W(U w, U h)
+    WN(U w, U h)
         : w { w }
         , h { h }
     {
@@ -155,133 +224,67 @@ struct W
         XCloseDisplay(d);
         o = false;
     }
-    #endif
 };
-
-#if PW
-LRESULT CALLBACK wp(HWND h, UINT m, WPARAM wp, LPARAM lp)
-{
-    A w = (W*)GetWindowLongPtr(h, GWL_USERDATA);
-    if (w) {
-        switch (m) {
-            case WM_CLOSE:w->c(); break;
-        }
-    }
-
-    return DefWindowProc(h, m, wp, lp);
-}
 #endif
 
-struct vec2
+static float scale = 2.2f;
+void onmouse(float d)
 {
-    float x;
-    float y;
-};
-
-float length(vec2 v)
-{
-    return sqrt(v.x * v.x + v.y * v.y);
+    scale -= d * 0.001f;
 }
 
 int main()
 {
-    W w(640, 480);
-    auto p = &w;
-    while (w.o) {
+    WN w(1024,1024);
+    w.s = onmouse;
+    while(w.o){
         w.u();
-        static bool rendered;
-        if (!rendered) {
-            for (size_t yi = 0; yi < w.h; ++yi) {
-                for (size_t xi = 0; xi < w.w; ++xi) {
-                    vec2 texcoord { (float)xi / (float)w.w, (float)yi / (float)w.h };
-                    {
-                        float scale = 2.2f;
-                        vec2 center { 0.7f, 0 };
-                        size_t iterations = 1000;
+        //Sleep(32);
+        for(U yi=0;yi<w.h;++yi)
+            for(U xi=0;xi<w.w;++xi){
+                vec2 t{xi/F(w.w),yi/F(w.h)};
 
-                        vec2 z, c;
-                        c.x = 1.3333f * (texcoord.x - 0.5f) * scale - center.x;
-                        c.y =           (texcoord.y - 0.5f) * scale - center.y;
+                // F scale = 2.2f;
+                vec2 center { 0.7f, 0 };
+                U iterations = 64;
 
-                        z = c;
-                        size_t i = 0;
-                        for (; i < iterations; ++i) {
-                            float x = (z.x * z.x - z.y * z.y) + c.x;
-                            float y = (z.y * z.x + z.x * z.y) + c.y;
-                            if ((x * x + y * y) > 4) {
-                                break;
-                            }
+                vec2 z, c;
+                c[0] = 1.3333f * (t[0] - 0.5f) * scale - center[0];
+                c[1] =           (t[1] - 0.5f) * scale - center[1];
 
-                            z.x = x;
-                            z.y = y;
-                        }
-
-                        float value = float(i) / 100.0f;
-                        
-                        #if PW
-                        COLORREF color =
-                            i == iterations ?
-                            RGB(0, 0, 0) :
-                            RGB((uint8_t)(value * 255.0f), 1, 1);
-
-                        SetPixel(w.dc, xi, yi, color);
-                        #endif
-                        
-                        #if PL
-                        uint8_t r =
-                            i == iterations ?
-                            0 :
-                            (uint8_t)(value * 255.0f);
-                        uint8_t g = 0;
-                        uint8_t b = 0;
-                        unsigned long color = ((r& 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
-                        XSetForeground(w.d, w.gc, color);
-                        XDrawPoint(w.d, w.hw, w.gc, xi, yi);
-                        #endif
+                z = c;
+                U i = 0;
+                for (; i < iterations; ++i) {
+                    F x = (z[0] * z[0] - z[1] * z[1]) + c[0];
+                    F y = (z[1] * z[0] + z[0] * z[1]) + c[1];
+                    if ((x * x + y * y) > 4) {
+                        break;
                     }
-                }
-            }
 
-            rendered = true;
-        }
+                    z[0] = x;
+                    z[1] = y;
+                }
+
+                F value = F(i) / 100.0f;
+                        
+                #if PW
+                w.f.p(xi,yi,i==iterations?vec3{0,0,0}:vec3{value,0,0});
+                #endif
+
+                #if PL
+                uint8_t r =
+                    i == iterations ?
+                    0 :
+                    (uint8_t)(value * 255.0f);
+                uint8_t g = 0;
+                uint8_t b = 0;
+                unsigned long color = ((r& 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+                XSetForeground(w.d, w.gc, color);
+                XDrawPoint(w.d, w.hw, w.gc, xi, yi);
+                #endif
+
+            }
     }
 
-    return 0;
+    R 0;
 }
-
-/*
-
-A : auto
-B :
-C :
-D :
-E :
-F : float
-G :
-H :
-I : int
-J :
-K :
-L :
-M :
-N :
-O :
-P :
-Q :
-R :
-S :
-T :
-U : uint32_t
-V :
-W : struct Window
-X :
-Y :
-Z : size_t
-
-PW : Platform Windows
-PL : Platform Linux
-
-sudo apt-get install libx11-dev
-reset; g++ -std=c++11 -Wall Main.cpp -o MiniWindow -lX11; ./MiniWindow
-
-*/
---
+++ MiniGLM.hpp
@@ -0,0 +1,222 @@
+
+#pragma once
+
+#include <array>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+#include "Defines.hpp"
+
+namespace glm {
+    template <size_t C>
+    using vec = std::array<float, C>;
+    using vec2 = vec<2>;
+    using vec3 = vec<3>;
+    using vec4 = vec<4>;
+
+    #define VEC_OPERATOR(OPERATOR)                                 \
+    template <Z C>                                                 \
+    vec<C> operator OPERATOR(const vec<C>& lhs, const vec<C>& rhs) \
+    {                                                              \
+        vec<C> r;                                                  \
+        for (Z i = 0; i < C; ++i)                                  \
+            r[i] = lhs[i] OPERATOR rhs[i];                         \
+        R r;                                                       \
+    }                                                              \
+                                                                   \
+    template <size_t C>                                            \
+    vec<C> operator OPERATOR (const vec<C>& lhs, float scalar)     \
+    {                                                              \
+        vec<C> r;                                                  \
+        for (Z i = 0; i < C; ++i)                                  \
+            r[i] = lhs[i] OPERATOR scalar;                         \
+        R r;                                                       \
+    }
+
+    VEC_OPERATOR(+)
+    VEC_OPERATOR(-)
+    VEC_OPERATOR(*)
+    VEC_OPERATOR(/)
+
+    template <size_t C>
+    float dot(const vec<C>& v0, const vec<C>& v1)
+    {
+        float result = 0;
+        for (size_t i = 0; i < C; ++i)
+            result += v0[i] * v1[i];
+        R result;
+    }
+
+    template <size_t C>
+    float length(const vec<C>& v)
+    {
+        R dot(v, v);
+    }
+
+    template <size_t C>
+    vec<C> normalize(const vec<C>& vec)
+    {
+        R vec / std::sqrt(dot(vec, vec));
+    }
+
+    vec3 cross(const vec3& v0, const vec3& v1)
+    {
+        R vec3 {
+            v0[1] * v1[2] - v1[1] * v0[2],
+            v0[2] * v1[0] - v1[2] * v0[0],
+            v0[0] * v1[1] - v1[0] * v0[1]
+        };
+    }
+
+    using mat4 = std::array<vec4, 4>;
+
+    mat4 operator*(const mat4& lhs, const mat4& rhs)
+    {
+        const auto SrcA0 = lhs[0];
+        const auto SrcA1 = lhs[1];
+        const auto SrcA2 = lhs[2];
+        const auto SrcA3 = lhs[3];
+
+        const auto SrcB0 = rhs[0];
+        const auto SrcB1 = rhs[1];
+        const auto SrcB2 = rhs[2];
+        const auto SrcB3 = rhs[3];
+
+        mat4 Result;
+        Result[0] = SrcA0 * SrcB0[0] + SrcA1 * SrcB0[1] + SrcA2 * SrcB0[2] + SrcA3 * SrcB0[3];
+        Result[1] = SrcA0 * SrcB1[0] + SrcA1 * SrcB1[1] + SrcA2 * SrcB1[2] + SrcA3 * SrcB1[3];
+        Result[2] = SrcA0 * SrcB2[0] + SrcA1 * SrcB2[1] + SrcA2 * SrcB2[2] + SrcA3 * SrcB2[3];
+        Result[3] = SrcA0 * SrcB3[0] + SrcA1 * SrcB3[1] + SrcA2 * SrcB3[2] + SrcA3 * SrcB3[3];
+        R Result;
+
+        // mat4 result;
+        // result[0] = lhs[0] * rhs[0] + lhs[1] * rhs[0] + lhs[2] * rhs[0] + lhs[3] * rhs[0];
+        // result[1] = lhs[0] * rhs[1] + lhs[1] * rhs[1] + lhs[2] * rhs[1] + lhs[3] * rhs[1];
+        // result[2] = lhs[0] * rhs[2] + lhs[1] * rhs[2] + lhs[2] * rhs[2] + lhs[3] * rhs[2];
+        // result[3] = lhs[0] * rhs[3] + lhs[1] * rhs[3] + lhs[2] * rhs[3] + lhs[3] * rhs[3];
+        // R result;
+    }
+
+    vec<4> operator*(const mat4& m, const vec4& v)
+    {
+        R vec<4> {
+            m[0][0] * v[0] + m[1][0] * v[1] + m[2][0] * v[2] + m[3][0] * v[3],
+            m[0][1] * v[0] + m[1][1] * v[1] + m[2][1] * v[2] + m[3][1] * v[3],
+            m[0][2] * v[0] + m[1][2] * v[1] + m[2][2] * v[2] + m[3][2] * v[3],
+            m[0][3] * v[0] + m[1][3] * v[1] + m[2][3] * v[2] + m[3][3] * v[3]
+        };
+    }
+
+    mat4 create_identity()
+    {
+        R mat4 {
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        };
+    }
+
+    mat4 lookAt(const vec3& eye, const vec3& center, const vec3& up)
+    {
+        auto f = normalize(center - eye);
+        auto s = normalize(cross(f, up));
+        auto u = cross(s, f);
+        auto result = create_identity();
+        result[0][0] = s[0];
+        result[1][0] = s[1];
+        result[2][0] = s[2];
+        result[0][1] = u[0];
+        result[1][1] = u[1];
+        result[2][1] = u[2];
+        result[0][2] = -f[0];
+        result[1][2] = -f[1];
+        result[2][2] = -f[2];
+        result[3][0] = -dot(s, eye);
+        result[3][1] = -dot(u, eye);
+        result[3][2] = dot(f, eye);
+        R result;
+    }
+
+    mat4 perspective(float fovy, float aspect, float zNear, float zFar)
+    {
+        float tanHalfFov = std::tan(fovy * 0.5f);
+        mat4 result { };
+        result[0][0] = 1.0f / (tanHalfFov * aspect);
+        result[1][1] = 1.0f / tanHalfFov;
+        result[2][3] = -1;
+        result[2][2] = zFar / (zNear - zFar);
+        result[3][2] = -(zFar * zNear) / (zFar - zNear);
+        R result;
+    }
+
+    template <typename RV, typename T>
+    RV round_cast(const T& value)
+    {
+        R static_cast<RV>(std::round(value));
+    }
+
+    template <typename T>
+    const T& clamp(const T& v, const T& n, const T& x)
+    {
+        R std::min(std::max(n, v), x);
+    }
+
+    template <typename T>
+    T lerp(const T& v0, const T& v1, const T& t)
+    {
+        R std::fma(t, v1, std::fma(-t, v0, v0));
+    }
+
+    template <Z C>
+    vec<C> saturate(vec<C> v)
+    {
+        vec<C> r;
+        for (Z i = 0; i < C; ++i)
+            r[i] = clamp<F>(v[i], 0, 1);
+        R r;
+    }
+
+    template <typename T>
+    T pi()
+    {
+        return T(3.14159265358979323846264338327950288);
+    }
+
+    template <typename T>
+    T radians(const T& degrees)
+    {
+        R pi<T>() / static_cast<T>(180.0) * degrees;
+    }
+
+    template <typename T>
+    T degrees(const T& radians)
+    {
+        R static_cast<T>(180.0) / pi<T>() * radians;
+    }
+
+    template <size_t C>
+    std::string to_string(const vec<C>& v)
+    {
+        std::string str;
+        for (size_t i = 0; i < C; ++i) {
+            str += std::to_string(v[i]);
+            if (i < C - 1) {
+                str += ", ";
+            }
+        }
+
+        R "{ " + str + " }";
+    }
+
+    std::string to_string(const mat4& m)
+    {
+        R   "{" +
+            to_string(m[0]) + "\n " +
+            to_string(m[1]) + "\n " +
+            to_string(m[2]) + "\n " +
+            to_string(m[3]) +
+            "}";
+    }
+} // namespace glm
