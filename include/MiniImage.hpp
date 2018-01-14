101115fd565d258b6da1f532b56058ececcddacc
Patrick Purcell <p.84@live.com>
2018-01-14 05:03:13 -0800

    Golfing MiniImage

--- include/MiniImage.hpp
+++ include/MiniImage.hpp
@@ -12,6 +12,9 @@
 #include "MiniDefines.hpp"
 #include "MiniGLM_ex.hpp"
 
+// #define MINI_IMAGE_UNGOLFED
+#if defined MINI_IMAGE_UNGOLFED||defined MINI_RENDERER_UNGOLFED
+
 /**
  * TODO : Documentation.
  */
@@ -137,3 +140,27 @@ V xWu(Img<T>&img,VT v0,VT v1,T c)
             intrcpt+=grdnt;
         }
 }
+
+#else
+
+TMP<TYP T>class Img:public vector<T>{public:I w,h;Img(I w,I h){rsz(w,h);}V rsz(I
+_w,I _h){w=_w;h=_h;resize(w*h);}T&pxl(I x,I y){static T n;R 0<=x&&x<w&&0<=y&&y<h
+?this->O[](y*w+x):n;}TMP<I N>A nrmlz(vc<N>&v){A r=v;r[0]=lrp(w*.5f,(F)w,v[0]);r[
+1]=lrp(h*.5f,(F)h,-v[1]);R r;}A prjct(vc4&v){R nrmlz(v/v[3]);}V clr(T c={}){fill
+(begin(),end(),c);}};F xWuF(F f){f-=floor(f);R f<0?1-f:f;}F xWuRF(F f){R 1-xWuF(
+f);}TMP<TYP T,TYP VT>I xWuE(Img<T>&img,VT&v,I steep,F grdnt,T&c,F*intrcpt=0){I
+xe=(I)round(v[0]);F ye=v[1]+grdnt*(xe-v[0]);F gap=xWuRF(v[0]+.5f);I xpxl1=xe;I
+ypxl1=(I)ye;if(steep){img.pxl(xpxl1,ypxl1)+=c*xWuRF(ye)*gap;img.pxl(xpxl1,ypxl1+
+1)+=c*xWuF(ye)*gap;}else{img.pxl(ypxl1,xpxl1)+=c*xWuRF(ye)*gap;img.pxl(ypxl1,
+xpxl1+1)+=c*xWuF(ye)*gap;}if(intrcpt)*intrcpt=ye+grdnt;R xpxl1;}TMP<TYP T,TYP VT
+>V xWu(Img<T>&img,VT v0,VT v1,T c){swap(v0[0],v0[1]);swap(v1[0],v1[1]);F x0=v0[0
+];F y0=v0[1];F x1=v1[0];F y1=v1[1];I steep=abs(y1-y0)>abs(x1-x0);if(steep){swap(
+v0[0],v0[1]);swap(v1[0],v1[1]);}if(v0[0]>v1[0]){swap(v0[0],v1[0]);swap(v0[1],v1[
+1]);}F intrcpt=0;A dlt=v1-v0;F grdnt=dlt[0]?dlt[1]/dlt[0]:1;I xpxl1=xWuE(img,v0,
+steep,grdnt,c,&intrcpt);I xpxl2=xWuE(img,v1,steep,grdnt,c);if (steep)for(I x=
+xpxl1+1;x<xpxl2;++x){img.pxl(x,(I)intrcpt)+=c*xWuRF(intrcpt);img.pxl(x,(I)
+intrcpt+1)+=c*xWuF(intrcpt);intrcpt+=grdnt;}else for(I x=xpxl1+1;x<xpxl2;++x){
+img.pxl((I)intrcpt,x)+=c*xWuRF(intrcpt);img.pxl((I)intrcpt+1,x)+=c*xWuF(intrcpt)
+;intrcpt+=grdnt;}}
+
+#endif
